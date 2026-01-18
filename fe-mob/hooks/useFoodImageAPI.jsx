cd2b820afed271f06775b91e88907b0494cb9ef6
Rafael05 <domagasraprap@gmail.com>
2026-01-18 23:36:55 +0800

    Changed the ip to render

--- fe-mob/hooks/useFoodImageAPI.jsx
+++ fe-mob/hooks/useFoodImageAPI.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const API_BASE_URL = 'http://192.168.8.34:5000';
+const API_BASE_URL = 'https://nutriliz-be.onrender.com';
 
 export const useFoodImageAPI = () => {
   const [loading, setLoading] = useState(false);
