5b53fae0910fdf62ab4fc65a71d972d462db5278
Rafael05 <domagasraprap@gmail.com>
2026-01-19 23:30:09 +0800

    4th push

--- fe-web-comp/src/routes/index.lazy.jsx
+++ fe-web-comp/src/routes/index.lazy.jsx
@@ -7,7 +7,7 @@ export const Route = createLazyFileRoute('/')({
 function Index() {
   return (
     <div className="p-2">
-      <h3 className='text-3xl font-bold underline'>Welcome Home</h3>
+      <h3 className='text-3xl font-bold underline'>Welcome Homeiouhfiosdf</h3>
     </div>
   )
 }
